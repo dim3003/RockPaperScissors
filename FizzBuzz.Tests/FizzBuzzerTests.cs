57095053f2c987baf67c2f66c2e36c9fbd63e792
dim3003 <andre.d3003@gmail.com>
2026-03-18 18:27:17 +0100

    fizzbuzz start

---
+++ FizzBuzz.Tests/FizzBuzzerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz.Tests;
+
+[TestFixture]
+public class FizzBuzzerTests
+{
+    [TestFixture]
+    public class Go
+    {
+        [TestFixture]
+        public class WhenNumberDivisibleBy3
+        {
+            [TestCase(3)]
+            [TestCase(21)]
+            [TestCase(99)]
+            public void ShouldReturnFizz(int number)
+            {
+                // Arrange
+                var sut = new FizzBuzzer();
+                // Act
+                var actual = sut.Go(number);
+                // Assert
+                Assert.That(actual, Is.EqualTo("Fizz"));
+            }
+        }
+    }
+
+}
---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,13 @@
+﻿namespace FizzBuzz;
+
+public class FizzBuzzer
+{
+    public string Go(int number)
+    {
+        if (number % 3 == 0)
+        {
+            return "Fizz";
+        }
+        return null;
+    }
+}
